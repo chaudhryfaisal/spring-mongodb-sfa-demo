e71594da3ba0036340dc600b23d94b924e75be74
Faisal Chaudhry <faisal.chaudhry@mastercard.com>
2018-04-19 00:59:23 -0500

    updated reset script

--- scripts/01_reset.sh
+++ scripts/01_reset.sh
@@ -14,8 +14,6 @@ reset_ns(){
     echo "reset_ns"
     recreate_ns "staging"
     recreate_ns "released"
-    sleep 5
-    kubectl get ns
 
 }
 
@@ -25,11 +23,12 @@ recreate_ns(){
  delete=true
  while ! (kubectl get ns $name 2>&1 | grep -q "NotFound");do
  if [ "$delete" = true ]; then delete=false; kubectl delete ns $name; fi
-    echo -n "."
-    sleep 5;
+    echo -n "." ; sleep 5;
  done
  echo -e "\nCreating Namespace $name"
  kubectl create ns $name
+ while (kubectl get ns $name 2>&1 | grep -q "invalid");do echo -n ".";sleep 1; done
+ echo ""
 }
 
 main
