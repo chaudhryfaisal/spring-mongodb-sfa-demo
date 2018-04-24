61a5d9228e0c013cf64689598fea85d883283a46
Faisal Chaudhry <faisal.chaudhry@mastercard.com>
2018-04-24 17:01:05 -0400

    Include app build info into headers

--- api-service/src/main/java/example/controller/HeaderModifierAdvice.java
+++ api-service/src/main/java/example/controller/HeaderModifierAdvice.java
@@ -1,7 +1,10 @@
 package example.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
@@ -19,16 +22,34 @@ public class HeaderModifierAdvice implements ResponseBodyAdvice<Object> {
     @Value("${headers.prefix:app_}")
     String headersPrefix;
 
+    @Value("${git.build.version:}")
+    String buildVersion;
+
+    @Value("${git.build.time:}")
+    String buildTime;
+
+    @Value("${git.branch:}")
+    String buildBranch;
+
+    @Value("${git.commit.id.abbrev:}")
+    String buildTag;
+
     private HashMap<String, String> headers = new HashMap<>();
 
     @PostConstruct
     public void init() {
+        headersPrefix = headersPrefix.toUpperCase();
+        headers.put(headersPrefix + "BUILD_VER", buildVersion);
+        headers.put(headersPrefix + "BUILD_TIME", buildTime);
+        headers.put(headersPrefix + "BUILD_TAG", buildTag);
+        headers.put(headersPrefix + "BUILD_BRANCH", buildBranch);
+        headers.put("HOSTNAME", System.getenv("HOSTNAME"));
+
         for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
-            if (entry.getKey().toLowerCase().startsWith(headersPrefix)) {
-                headers.put(entry.getKey(), entry.getValue());
+            if (entry.getKey().toUpperCase().startsWith(headersPrefix)) {
+                headers.put(entry.getKey().toUpperCase(), entry.getValue());
             }
         }
-        headers.put("HOSTNAME", System.getenv("HOSTNAME"));
     }
 
     @Override
@@ -45,5 +66,14 @@ public class HeaderModifierAdvice implements ResponseBodyAdvice<Object> {
                                   final ServerHttpResponse response) {
         headers.forEach((key, value) -> response.getHeaders().add(key, value));
         return body;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
     }
 }
